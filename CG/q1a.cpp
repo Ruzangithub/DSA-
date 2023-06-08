e91952a231d349be1ed1d994f6a7b489bcf6fbb0
Ruzangithub <118183445+Ruzangithub@users.noreply.github.com>
2023-06-08 11:40:14 +0545

    minor

--- CG/q1a.cpp
+++ CG/q1a.cpp
@@ -4,15 +4,17 @@ using namespace std;
 
 int main()
 {
-    for(int i=0; i<=4;i++)
+    for(int i=1; i<=5;i++)
     {
-        for(int j=0;j<i;j++)
-    {
+        for(int k=5-i;k>0;k--){
+            cout<<" ";
+        }
+    for(int j=1;j<=i;j++){
         cout<<"*";
     }
       cout<<endl;  
     }
-    
+    return 0;
 } 
 //hello world
 //no push
--- CG/q2.cpp
+++ CG/q2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    int arr[]={1,2,3,4,5,6,7,8,9,10};
+    int arr[]={24,-43,5,2,4,6,23,2,12,344};
     int largest=0;
     int seclargest=0;
     for(int i=0; i<10;i++)
