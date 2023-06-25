52bdd631da1c5e4cfa358c2db2714b4c860f43b0
Ruzangithub <118183445+Ruzangithub@users.noreply.github.com>
2023-06-25 21:52:19 +0545

    ...

--- DSA/LAB2/Q3.c
+++ DSA/LAB2/Q3.c
@@ -4,28 +4,34 @@
 
 #define MAX_SIZE 100
 
-int Empty(int top) {
+int Empty(int top) 
+{
     return top == -1;
 }
 
-void push(char stack[], int* top, char item) {
+void push(char stack[], int* top, char item) 
+{
     stack[++(*top)] = item;
 }
 
-char pop(char stack[], int* top) {
-    if (Empty(*top)) {
+char pop(char stack[], int* top) 
+{
+    if (Empty(*top)) 
+    {
         printf("Stack Underflow\n");
         return '\0';
     }
     return stack[(*top)--];
 }
 
-void infToPof(char infix[], char postfix[]) {
+void infToPof(char infix[], char postfix[]) 
+{
     char stack[MAX_SIZE];
     int top = -1;
     int i, j = 0;
 
-    for (i = 0; infix[i] != '\0'; i++) {
+    for (i = 0; infix[i] != '\0'; i++) 
+    {
         char ch = infix[i];
 
         if (isalpha(ch) || isdigit(ch))
@@ -35,21 +41,24 @@ void infToPof(char infix[], char postfix[]) {
         else if (ch == ')') {
             while (!Empty(top) && stack[top] != '(')
                 postfix[j++] = pop(stack, &top);
-            if (!Empty(top) && stack[top] != '(') {
+            if (!Empty(top) && stack[top] != '(') 
+            {
                 printf("Invalid expression\n");
                 return;
             } else {
                 pop(stack, &top);
             }
         } else {
-            while (!Empty(top) && stack[top] != '(' && (ch == '+' || ch == '-' || ch == '*' || ch == '/')) {
+            while (!Empty(top) && stack[top] != '(' && (ch == '+' || ch == '-' || ch == '*' || ch == '/')) 
+            {
                 postfix[j++] = pop(stack, &top);
             }
             push(stack, &top, ch);
         }
     }
 
-    while (!Empty(top)) {
+    while (!Empty(top)) 
+    {
         if (stack[top] == '(') {
             printf("Invalid expression\n");
             return;
@@ -60,7 +69,8 @@ void infToPof(char infix[], char postfix[]) {
     postfix[j] = '\0';
 }
 
-int main() {
+int main() 
+{
     char infix[MAX_SIZE];
     char postfix[MAX_SIZE];
 
--- DSA/LAB2/Q4.c
+++ DSA/LAB2/Q4.c
@@ -1,1 +1,28 @@
 //Write a program to convert an infix expression to prefix using a stack.
+#include<stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define MAX_SIZE 100
+
+int Empty(int top) 
+{
+    return top == -1;
+}
+
+void push(char stack[], int* top, char item) 
+{
+    stack[++(*top)] = item;
+}
+
+char pop(char stack[], int* top) 
+{
+    if (Empty(*top)) 
+    {
+        printf("Stack Underflow\n");
+        return '\0';
+    }
+    return stack[(*top)--];
+}
+
+
